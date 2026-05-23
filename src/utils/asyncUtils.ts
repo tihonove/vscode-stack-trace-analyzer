5d3ee50b6281e581539f5dbd95c4c88689919cb3
Eugene Tihonov <tihonov.ea@gmail.com>
2026-05-23 17:01:45 +0200

    feat: add withTimeout utility

--- src/utils/asyncUtils.ts
+++ src/utils/asyncUtils.ts
@@ -1,3 +1,7 @@
 export function delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+    return Promise.race([promise, delay(ms).then(() => Promise.reject(new Error(`Timeout after ${ms}ms`)))]);
+}
